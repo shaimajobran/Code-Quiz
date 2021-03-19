c20096473e5bd5271fa31263da82b4f743a868a1
shaimaJobran <shaimajobran222gmail.com>
2021-03-19 11:36:54 -0400

    updating our details

---
+++ README.md
@@ -0,0 +1,18 @@
+# CODE Quiz 
+
+##
+This application is an HTML document ,styled with CSS,and is functioning using javaScript .
+
+It is a timed qiz on javaScript that stores high scores in localstorage.
+
+## Decription
+```
+When I click start button timer starts 
+And am presented with a question  
+When I answer the question am presented by another question 
+When the question is answered incorrectly, ten second is subtracted from the clock.
+when all questions are answered Game is finished 
+Then I can save my score an initials in localstorage
+```
+
+## screenshot
---
+++ script.js
@@ -0,0 +1,178 @@
+
+var header = document.getElementById("header");
+var intro = document.getElementById("intro");
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+var choices = document.getElementById("choices");
+var choice1 = document.getElementById("1");
+var choice2 = document.getElementById("2");
+var choice3 = document.getElementById("3");
+var choice4 = document.getElementById("4");
+var finalScore = document.getElementById("finalScore");
+var endMessage = document.getElementById("endMessage");
+var finalAnswer = document.getElementById("finalAnswer");
+var score = document.getElementById("score");
+
+
+//The array of questions 
+var questions = [{
+  question: "What is the name of CSS design that calls for fluid and adaptable elements based on the device resolution or size?",
+  choice1: "Responsive",
+  choice2: "cascading",
+  choice3: "evolution",
+  choice4: "shifting",
+  correctAnswer: "1"
+},
+
+ { question: "CSS can be used to arrange or organize images into a ____.",
+    choice1: "Gallery",
+      choice2: "Table",
+        choice3: "Shadow Box",
+          choice4: "Boxes",
+            correctAnswer: "4"
+},
+
+ { question: "What is used primarily to add styling to a web page?",
+    choice1: "HTML",
+      choice2: "CSS",
+        choice3: "Python",
+          choice4: "React.js",
+            correctAnswer: "2"
+},
+
+ {question: "What HTML attribute references an external JavaScript file?",
+    choice1: "href",
+      choice2: "src",
+        choice3: "class",
+          choice4: "index",
+            correctAnswer: "2"
+},
+
+
+]
+
+intro.style.display = "block";
+quiz.style.display = "none";
+finalScore.style.display = "none";
+
+// Start Quiz Button
+var startBtn = document.getElementById("startBtn");
+
+//  click of "Start Quiz" button
+startBtn.addEventListener("click", startGame);
+
+
+// Timer  Begin
+var timeLeft = 30;
+var startScore = 0;
+var timer = document.getElementById("timer");
+
+timer.textContent = "Time: " + startScore + "s";
+
+// Start Game function
+function startGame() {
+  quiz.style.display = "block";
+  question.style.display = "block";
+  header.style.display = "block";
+  intro.style.display = "none";
+  finalScore.style.display = "none";
+
+
+  var timeInterval = setInterval(function () {
+    timer.textContent = "Time:" + timeLeft + "s";
+    timeLeft -= 1;
+
+    if (timeLeft === 0 || questions.length === runningQuestionIndex + 1) {
+      resultRender();
+      clearInterval(timeInterval);
+      timer.textContent = "Time:" + timeLeft + "s";
+    }
+  }, 1000);
+
+  renderQuestion();
+};
+
+// show Questions 
+var lastQuestionIndex = questions.length - 1;
+var runningQuestionIndex = 0;
+
+function renderQuestion() {
+  var q = questions[runningQuestionIndex];
+  question.innerHTML = q.question;
+  choice1.innerHTML = q.choice1;
+  choice2.innerHTML = q.choice2;
+  choice3.innerHTML = q.choice3;
+  choice4.innerHTML = q.choice4;
+};
+
+// Check Answers
+function checkAnswer(answer) {
+  if (questions[runningQuestionIndex].correctAnswer == answer) {
+    answerOutput.textContent = "Correct!"
+  }
+  else {
+    answerOutput.textContent = "Wrong!"
+    timeLeft -= 10;
+  }
+// If it went through all questions, show final score
+  if (questions.length === runningQuestionIndex + 1) {
+    resultRender(); 
+    return;
+  }
+  runningQuestionIndex++;
+  renderQuestion();
+};
+
+//Score Quiz
+function resultRender() {
+  quiz.style.display = "none";
+  intro.style.display = "none";
+  finalScore.style.display = "block";
+
+  if (timeLeft === 0 || questions.length - 1) {
+    finalAnswer.textContent = "Your final score is " + timeLeft + ".";
+  }
+};
+
+// Score and Initials 
+userInformation.addEventListener("click", function () {
+  var contactInfo = document.getElementById("contactInfo").value;
+
+  localStorage.setItem("contactInfo", JSON.stringify(contactInfo));
+  localStorage.setItem("timeLeft", JSON.stringify(timeLeft));
+
+  loadScores();
+});
+
+
+var clearBtn = document.getElementById("clear");
+clearBtn.addEventListener("click", function() {
+    localStorage.clear();
+})
+
+var finalScore = {
+  timeleft: "30"
+}
+
+var finalScore = [];
+
+
+//show Scores 
+var showScores = function() {
+  var finalScore = localStorage.getItem("contactInfo, timeLeft");
+  
+      if (!finalScore) {
+          return false;
+      }
+    
+       for (var i = 0; i < finalScore.length; i++) {
+        var finalScore = finalScore[i];
+        var listItemEl = document.createElement("li");
+        listItemEl.textContent = finalScore;
+      }    
+    finalScore.push(finalScore)   
+};
+
+
+
+
